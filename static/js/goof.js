8088e775687f9c84bec7d1e49403c2185f1245a8
Léo Peltier <contact@leo-peltier.fr>
2015-10-19 12:09:34 +0200

    Serve backbone and underscore.

--- static/js/goof.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    "use strict";
-    var calendars = {};
-    document.goof = {calendars: calendars};
-
-    $.ajax('/calendar/calendar.ics').done(function(data) {
-        calendars["calendar.ics"] = data.Data.Calendars[0];
-    });
-})();
