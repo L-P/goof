948134db98d8e44080d696b32b70446ccf9f8d7f
Léo Peltier <leo@boaterfly.com>
2016-06-09 17:04:08 +0200

    Display events

--- static/js/goof/main.js
+++ static/js/goof/main.js
@@ -3,10 +3,31 @@
 document.goof = document.goof || {};
 
 $(function() {
+    function transformEvent(event) {
+        return {
+            title: event.Summary,
+            start: event.Start,
+            end: event.End,
+            raw: event
+        }
+    }
+
+    function getCalendarEventSource(calendarUrl) {
+        return function(start, end, timezone, callback) {
+            $.ajax({
+                url: calendarUrl,
+                data: {
+                    start: start.format("YYYY-MM-DD"),
+                    end: end.format("YYYY-MM-DD"),
+                },
+                success: function(data) {
+                    callback(_.map(data.Data.Calendar.Events, transformEvent));
+                }
+            });
+        };
+    }
+
     $('.js-calendar-body').fullCalendar({
-        // TODO: fetch dynamically
-        eventSources: [
-            '/calendar/calendar.ics',
-        ],
+        eventSources: getCalendarEventSource('/calendar/calendar.ics')
     });
 });
